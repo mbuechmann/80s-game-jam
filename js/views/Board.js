b3bf0caa56941583757877b44d180995fb875ca3
mbuechmann <malte.buechmann@gmail.com>
2014-07-12 13:23:30 +0200

    Clean up board

--- js/views/Board.js
+++ js/views/Board.js
@@ -3,12 +3,11 @@ var Board = function (game, x, y) {
     const PADDING = 4;
     var tileSize = 64;
 
-    var game = game;
     var group = game.add.group();
     group.x = x || 0;
     group.y = y || 0;
-    for(var i = 0; i < 6; i++) {
-        for(var j = 0; j < 5; j++) {
+    for (var i = 0; i < 6; i++) {
+        for (var j = 0; j < 5; j++) {
             if ((i === 1 || i === 4) && (j === 1 || j === 3)) {
                 group.create(i * (PADDING + tileSize), j * (PADDING + tileSize), 'board-hole');
             } else {

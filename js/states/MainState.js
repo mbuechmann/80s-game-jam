fbfb096424b8d565061497a96ae0a6118c4f9507
mbuechmann <malte.buechmann@gmail.com>
2014-07-12 15:54:56 +0200

    Add players' lamps

--- js/states/MainState.js
+++ js/states/MainState.js
@@ -1,11 +1,14 @@
 var mainState = function (game) {
 
-    const BOARD_TOP = 90;
+    const BOARD_TOP = 110;
     const PADDING = 4;
 
     var board = null;
     var homesLeft = [];
     var homesRight = [];
+
+    var playerLampLeft = null;
+    var playerLampRight = null;
 
     this.preload = function () {
         game.load.image('board-tile', 'images/board/tile.png');
@@ -16,14 +19,22 @@ var mainState = function (game) {
         game.load.image('home-door-selected', 'images/home/door-selected.png');
         game.load.image('home-lamp-on', 'images/home/lamp-on.png');
         game.load.image('home-lamp-off', 'images/home/lamp-off.png');
+
+        game.load.image('player-lamp-on', 'images/player/lamp-on.png');
+        game.load.image('player-lamp-off', 'images/player/lamp-off.png');
+        game.load.image('player-lamp-selected', 'images/player/lamp-selected.png');
     };
 
     this.create = function () {
         board = new Board(game, 116, BOARD_TOP, PADDING);
+
         for (var i = 0; i < 5; i++) {
             homesLeft.push(new Home(game, 20, BOARD_TOP + i * (64 + PADDING), false));
             homesRight.push(new Home(game, 616, BOARD_TOP + i * (64 + PADDING), true));
         }
+
+        playerLampLeft = new PLayerLamp(game, 10, 20);
+        playerLampRight = new PLayerLamp(game, 554, 20);
     };
 
     this.update = function () {
---
+++ js/views/PlayerLamp.js
@@ -0,0 +1,13 @@
+var PLayerLamp = function (game, x, y) {
+
+    var group = game.add.group();
+    group.x = x || 0;
+    group.y = y || 0;
+
+    group.create(20, 0, 'player-lamp-on');
+    var lampOff = group.create(20, 0, 'player-lamp-off');
+    lampOff.visible = false;
+    var selection = group.create(20, 0, 'player-lamp-selected');
+    selection.visible = false;
+
+};
